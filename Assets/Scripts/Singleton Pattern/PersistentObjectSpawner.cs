8ff389812b3dc7a34ba8a8a4f0f3fef3428b2b03
Petr Hampl <2007hampl@seznam.cz>
2021-11-17 11:50:09 +0100

    Added: Persistent Object Spawner (Better way to instantiate singleton)

---
+++ Assets/Scripts/Singleton Pattern/PersistentObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Singleton_Pattern
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [Tooltip("This prefab will only be spawned once and will persist between scenes.")]
+        [SerializeField] private GameObject persistentObjectPrefab = null;
+
+        private static bool _hasSpawned = false;
+
+        private void Awake()
+        {
+            if (_hasSpawned){return;}
+
+            SpawnPersistentObject();
+            
+            _hasSpawned = true;
+        }
+
+        private void SpawnPersistentObject()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Singleton Pattern/SceneReloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Singleton_Pattern
+{
+    public class SceneReloader : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
