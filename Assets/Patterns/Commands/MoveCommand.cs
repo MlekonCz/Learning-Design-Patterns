c886ec3e50f19a11fbdea424dbe012747459ac3a
Petr Hampl <2007hampl@seznam.cz>
2021-11-20 22:14:54 +0100

    Added: Command Pattern

---
+++ Assets/Patterns/Commands/MoveCommand.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Patterns.Commands
+{
+    public class MoveCommand : Command
+    {
+        private readonly Vector3 _destination;
+        private readonly NavMeshAgent _agent;
+
+        public MoveCommand(Vector3 destination, NavMeshAgent _agent)
+        {
+            _destination = destination;
+            this._agent = _agent;
+        }
+        public override void Execute()
+        {
+            _agent.SetDestination(_destination);
+        }
+
+        public override bool IsFinished
+        {
+            get { return _agent.remainingDistance <= 0.1f; }
+        }
+    }
+}
